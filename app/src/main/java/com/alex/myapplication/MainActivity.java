a5e705e40f7ff505533adf82faa5121332b65a50
azrael8576 <azrael8576love2008@gmail.com>
2018-11-23 18:12:45 +0800

    Day 11 - Activity life-cycle

--- app/src/main/java/com/alex/myapplication/MainActivity.java
+++ app/src/main/java/com/alex/myapplication/MainActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -12,6 +13,7 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = MainActivity.class.getSimpleName();
     TextView txtName;
     EditText edtName;
     Button btnConfirm;
@@ -39,6 +41,8 @@ public class MainActivity extends AppCompatActivity {
                 showClearDialog();
             }
         });
+        //正在創建活動。
+        Log.d(TAG, "onCreate://正在創建活動 ");
     }
 
 
@@ -67,5 +71,35 @@ public class MainActivity extends AppCompatActivity {
                         }
                     })
                     .show();
+    }
+    @Override
+    protected void onStart() {
+        super.onStart();
+        //活動即將變為可見。
+        Log.d(TAG, "onStart://活動即將變為可見 ");
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        //活動已變為可見（現在已“恢復”）。
+        Log.d(TAG, "onResume://活動已變為可見（現在已“恢復”） ");
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        //另一項活動正在關注（此活動即將“暫停”）。
+        Log.d(TAG, "onPause://另一項活動正在關注（此活動即將“暫停”） ");
+    }
+    @Override
+    protected void onStop() {
+        super.onStop();
+        //活動不再可見（現在已“停止”）
+        Log.d(TAG, "onStop://活動不再可見（現在已“停止”） ");
+    }
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        //活動即將被銷毀。
+        Log.d(TAG, "onDestroy://活動即將被銷毀。 ");
     }
 }
--- app/src/main/java/com/alex/myapplication/SecondActivity.java
+++ app/src/main/java/com/alex/myapplication/SecondActivity.java
@@ -3,9 +3,12 @@ package com.alex.myapplication;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 
 public class SecondActivity extends AppCompatActivity {
+
+    private static final String TAG = SecondActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,5 +20,37 @@ public class SecondActivity extends AppCompatActivity {
         //設定txtContent字串
         TextView txtContent = findViewById(R.id.txtContent);
         txtContent.setText(name);
+        //正在創建活動。
+        Log.d(TAG, "onCreate://正在創建活動 ");
+    }
+    @Override
+    protected void onStart() {
+        super.onStart();
+        //活動即將變為可見。
+        Log.d(TAG, "onStart://活動即將變為可見 ");
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        //活動已變為可見（現在已“恢復”）。
+        Log.d(TAG, "onResume://活動已變為可見（現在已“恢復”） ");
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        //另一項活動正在關注（此活動即將“暫停”）。
+        Log.d(TAG, "onPause://另一項活動正在關注（此活動即將“暫停”） ");
+    }
+    @Override
+    protected void onStop() {
+        super.onStop();
+        //活動不再可見（現在已“停止”）
+        Log.d(TAG, "onStop://活動不再可見（現在已“停止”） ");
+    }
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        //活動即將被銷毀。
+        Log.d(TAG, "onDestroy://活動即將被銷毀。 ");
     }
 }
